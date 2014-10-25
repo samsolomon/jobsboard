c70c563231a62171891b7930be5fb0c28675e3c2
samsolomon <samuelrobertsolomon@gmail.com>
2014-10-25 17:17:43 -0500

    initial commit

---
+++ db/migrate/20141025221327_create_jobs.rb
@@ -0,0 +1,12 @@
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :company
+      t.string :location
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
